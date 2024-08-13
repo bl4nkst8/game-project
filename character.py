4c5d6f8bbda5b80e41f94d8b47e63a14f6a3c08c
idkmelissa <ggwsdfsbfdg@gmail.com>
2024-08-13 16:42:23 +0200

    fixed movement system, added config file to store constants

--- character.py
+++ character.py
@@ -2,21 +2,29 @@ import pygame
 
 
 class Character:
-    def __init__(self, window, x, y, width, height, velocity):
+    def __init__(self, window, x, y, width, height, speed):
         self.window = window
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.vel = velocity
+        self.speed = speed
 
     def draw(self):
         pygame.draw.rect(self.window, (255, 0, 0), (self.x, self.y, self.width, self.height))
-        pygame.display.update()
 
-    def movement(self, dx, dy):
-        self.x += self.vel * dx
-        self.y += self.vel * dy
-  
+    def move(self, keys):
+        dx = 0
+        dy = 0
 
+        if pygame.K_w in keys or pygame.K_UP in keys:
+            dy = -1
+        if pygame.K_s in keys or pygame.K_DOWN in keys:
+            dy = 1
+        if pygame.K_d in keys or pygame.K_RIGHT in keys:
+            dx = 1
+        if pygame.K_a in keys or pygame.K_LEFT in keys:
+            dx = -1
 
+        self.x += self.speed * dx
+        self.y += self.speed * dy
