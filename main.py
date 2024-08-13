43e6c875857ac0c2aad7dd3e4c510f717ab4a27b
idkmelissa <ggwsdfsbfdg@gmail.com>
2024-08-13 15:47:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from character import Character
 from window import Window
 
 if __name__ == "__main__":
     window = Window()
-    character = Character(window.screen)
-    character.run()
     window.run()
