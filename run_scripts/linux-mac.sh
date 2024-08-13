0d33905b7e774b76cd961f22ea7580338b2eb02c
idkmelissa <ggwsdfsbfdg@gmail.com>
2024-08-13 16:52:41 +0200

    added run scripts for distribution

---
+++ run_scripts/linux-mac.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to the parent directory
+cd ..
+
+# Execute the Python script
+python main.py
---
+++ run_scripts/windows.bat
@@ -0,0 +1,7 @@
+@echo off
+
+:: Change directory to the parent directory
+cd ..
+
+:: Execute the Python script+
+start python main.py
